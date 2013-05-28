22e63cc1c52ebd3877dfe236bca03638d5796bbf
long nguyen <nguy1461@vandals.uidaho.edu>
2013-05-28 16:14:56 -0700

    ..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,11 @@ Rubyists::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
+  config.action_dispatch.rack_cache = {
+    :metastore    => Dalli::Client.new,
+    :entitystore  => 'file:tmp/cache/rack/body',
+    :allow_reload => false
+  }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
